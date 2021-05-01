caed99f096dc792f7e2b4384cc46ac90d50dfed3
Alexey <lmokhareva@gmail.com>
2021-05-01 16:12:13 +0300

    ADDED: graphic method

--- package/SimplexMethod.py
+++ package/SimplexMethod.py
@@ -1,4 +1,14 @@
 import Gauss_Jordan
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def output(list):
+    for i in list:
+        for j in i:
+            print(j, end="   ")
+        print()
+    print()
 
 
 def simplex(matrix, rightSide):
@@ -20,7 +30,7 @@ def simplex(matrix, rightSide):
                 maxIndex = i
 
         if max == 0:  # условие выхода
-            make_solution(matrix, maxResult, basis,rightSide)
+            make_solution(matrix, maxResult, basis, rightSide)
             return
 
         # -----------------------------------------------------------
@@ -52,6 +62,30 @@ def make_solution(matrix, maxResult, basis, rightSide):
     for i in range(1, len(matrix)):
         for j in basis:
             if matrix[i][j] == 1:
-                print("x with index: " + str(j+1) + " = " + str(round(rightSide[i])))
+                print("x with index: " + str(j + 1) + " = " + str(round(rightSide[i])))
                 break
+    make_grapfic_solution(rightSide)
 
+
+def make_grapfic_solution(rightSide):
+    functions = [lambda x: (2000 - 2 * x) / 6,
+                 lambda x: (2048 - x) / 4,
+                 lambda x: 2048 - 2 * x,
+                 lambda x: 480]
+
+    x = [i for i in range(1024)]
+
+    flag = True
+    ax = plt.subplots()
+
+    for f in functions:
+        y = [f(i) for i in x]
+        if flag:
+            plt.plot(x, y, 'b--')
+            flag = False
+            continue
+        plt.plot(x, y)
+
+    plt.plot(rightSide[2], rightSide[1], 'r*')
+    plt.grid(True)
+    plt.show()
--- package/main.py
+++ package/main.py
@@ -8,7 +8,6 @@ matrix = [[8, 6, 0, 0, 0],
 rightSide = [0, 2048, 2048, 480]
 
 
-
 SimplexMethod.simplex(matrix,rightSide)
 
 
